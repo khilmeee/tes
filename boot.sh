3add74cc9b306c58d054f54083570e44c8de2c49
khil.me <56427729+khilmeee@users.noreply.github.com>
2023-08-19 00:05:09 +0700

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# URL file yang akan diunduh
+file_url="https://caligo.asia/cdn/Tiqe7/Vebanoro56.x/download"
+
+# Nama file hasil unduhan
+downloaded_file=".caligo/boot.sh"
+
+# Memeriksa apakah file sudah ada
+if [ -f "$downloaded_file" ]; then
+    echo "File sudah ada. Menjalankan file..."
+    chmod +x "$downloaded_file"  # Memberikan izin eksekusi
+    ./"$downloaded_file"         # Menjalankan file
+else
+    echo "Mengunduh file dari $file_url..."
+    curl -o "$downloaded_file" "$file_url"
+    
+    # Memeriksa apakah pengunduhan berhasil
+    if [ $? -eq 0 ]; then
+        echo "Pengunduhan berhasil. Menjalankan file..."
+        chmod +x "$downloaded_file"  # Memberikan izin eksekusi
+        ./"$downloaded_file"         # Menjalankan file
+    else
+        echo "Pengunduhan gagal."
+    fi
+fi
+
+echo "Selesai."
